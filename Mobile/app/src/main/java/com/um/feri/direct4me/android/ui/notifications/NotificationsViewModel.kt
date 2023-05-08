12cd585bae4001fa5880d328bb489b68b60a6e8f
AnejBezjak <anej.bezjak@student.um.si>
2023-05-08 11:07:32 +0200

    Init commit android

---
+++ Mobile/app/src/main/java/com/um/feri/direct4me/android/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.um.feri.direct4me.android.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
