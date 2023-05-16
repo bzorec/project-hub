f2050412cf4d038a14fc7e247408809c13d8b353
Blaz Zorec <blaz.zorec@endava.com>
2023-05-16 15:51:30 +0200

    todo

--- Web/Direct4Me.Minimal.Api/Program.cs
+++ Web/Direct4Me.Minimal.Api/Program.cs
@@ -4,6 +4,8 @@ using Direct4Me.Repository;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.ConfigureRepositoryServices();
+
+//TODO:bzorec make this so it's doesn't uses magic to add services to collection
 builder.Services.AddEndpointDefinitions(typeof(EndpointDefinitionExtensions));
 
 var app = builder.Build();
