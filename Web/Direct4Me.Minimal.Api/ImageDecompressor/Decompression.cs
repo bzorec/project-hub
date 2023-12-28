2a5f5d57d096bdb55b1f73ef141bdfba46f46ce2
misel <misel.cucek@student.um.si>
2023-12-28 11:49:07 +0100

    Implemented ImageDecompressor.

---
+++ Web/Direct4Me.Minimal.Api/ImageDecompressor/Decompression.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+
+namespace ImageCompressorDecompressor;
+
+internal static class Decompression
+{
+    // Decode the BitArray and update the list C based on the given parameters
+    public static void Decode(this BitArray bits, ref List<int> cList, int low, int high)
+    {
+        while (high - low > 1)
+        {
+            if (cList[high] != cList[low])
+            {
+                var midpoint = (low + high) / 2;
+                var requiredBits = (int)Math.Ceiling(Math.Log(cList[high] - cList[low] + 1, 2));
+                var buffer = 0;
+
+                for (var i = 0; i < requiredBits; i++)
+                {
+                    var indexToCheck = ImageProcessingExtensions.MagnificationIndex + i;
+
+                    if (indexToCheck < 0 || indexToCheck >= bits.Length || i >= bits.Length) continue;
+
+                    if (bits[indexToCheck]) ImageProcessingExtensions.SetBit(ref buffer, requiredBits - 1 - i);
+                }
+
+                cList[midpoint] = cList[low] + buffer;
+                ImageProcessingExtensions.MagnificationIndex += requiredBits;
+
+                if (low < midpoint) Decode(bits, ref cList, low, midpoint);
+
+                if (midpoint < high)
+                {
+                    low = midpoint;
+                    continue;
+                }
+            }
+            else if (cList[high] == cList[low])
+            {
+                for (var i = low; i <= high && i < cList.Count; i++) cList[i] = cList[low];
+            }
+
+            break;
+        }
+    }
+
+    public static void ReorderBytes(this BitArray bits)
+    {
+        for (var i = 0; i < bits.Length; i += 8)
+        for (var z = 0; z < 4; z++)
+        {
+            var oppositeIndex = i + 7 - z;
+
+            if (oppositeIndex < bits.Length && i + z < bits.Length)
+                (bits[oppositeIndex], bits[i + z]) = (bits[i + z], bits[oppositeIndex]);
+        }
+    }
+
+
+    // Calculate list N based on the list C and the given length n
+    public static List<int> CalculateN(this List<int> cList, int n)
+    {
+        var calculateN = new List<int> { cList[0] };
+
+        for (var i = 1; i < n; i++) calculateN.Add(cList[i] - cList[i - 1]);
+
+        return calculateN;
+    }
+
+    // Decode list N and return list E
+    public static List<int> DecodeE(this List<int> nList)
+    {
+        var eList = new List<int> { nList[0] };
+        eList.AddRange(nList.Skip(1).Select(i => i % 2 == 0 ? i / 2 : -(i + 1) / 2));
+        return eList;
+    }
+}
---
+++ Web/Direct4Me.Minimal.Api/ImageDecompressor/ImageProcessingExtensions.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Drawing;
+
+namespace ImageCompressorDecompressor;
+
+public static class ImageProcessingExtensions
+{
+    public static int MagnificationIndex = 76;
+
+    // Check if the bit at the specified position is set
+    public static bool IsBitSet(this int value, int position)
+    {
+        return position is < 0 or >= 32
+            ? throw new ArgumentOutOfRangeException(nameof(position))
+            : (value & (1 << position)) != 0;
+    }
+
+    // Set the bit at the specified position in the number
+    public static void SetBit(ref int number, int position)
+    {
+        if (position is < 0 or >= 32) throw new ArgumentOutOfRangeException(nameof(position));
+
+        number |= 1 << position;
+    }
+
+    // Reverse the prediction to reconstruct the image based on list E
+    public static Bitmap ReversePredict(IReadOnlyList<int> e, int height, int width)
+    {
+        var image = new Bitmap(width, height);
+
+        for (var x = 0; x < width; x++)
+        for (var y = 0; y < height; y++)
+        {
+            var value = e[x * height + y];
+
+            Color color;
+
+            if (x == 0 && y == 0)
+            {
+                color = Color.FromArgb(Clamp(value), Clamp(value), Clamp(value));
+            }
+            else if (y == 0)
+            {
+                var x1 = Clamp(image.GetPixel(x - 1, 0).R - value);
+                color = Color.FromArgb(x1, x1, x1);
+            }
+            else if (x == 0)
+            {
+                var y1 = Clamp(image.GetPixel(0, y - 1).R - value);
+                color = Color.FromArgb(y1, y1, y1);
+            }
+            else
+            {
+                int leftNeighbour = image.GetPixel(x - 1, y).R;
+                int upNeighbour = image.GetPixel(x, y - 1).R;
+                int diagonalNeighbour = image.GetPixel(x - 1, y - 1).R;
+
+                var max = Math.Max(leftNeighbour, upNeighbour);
+                var min = Math.Min(leftNeighbour, upNeighbour);
+
+                if (diagonalNeighbour >= max)
+                {
+                    color = Color.FromArgb(Clamp(min - value), Clamp(min - value),
+                        Clamp(min - value));
+                }
+                else if (diagonalNeighbour <= min)
+                {
+                    color = Color.FromArgb(Clamp(max - value), Clamp(max - value),
+                        Clamp(max - value));
+                }
+                else
+                {
+                    var tmp = leftNeighbour + upNeighbour - diagonalNeighbour;
+                    color = Color.FromArgb(Clamp(tmp - value), Clamp(tmp - value), Clamp(tmp - value));
+                }
+            }
+
+            image.SetPixel(x, y, color);
+        }
+
+        return image;
+    }
+
+    // Clamp the value between 0 and 255
+    public static int Clamp(int value)
+    {
+        return Math.Max(0, Math.Min(value, 255));
+    }
+
+    // Set the header for the compressed image
+    public static BitArray SetHeader(ushort height, byte c0, int cl, int n)
+    {
+        var bitArray = new BitArray(76);
+        var index = 0;
+
+        for (var j = 11; j >= 0; j--, index++) bitArray[index] = IsBitSet(height, j);
+
+        for (var j = 7; j >= 0; j--, index++) bitArray[index] = IsBitSet(c0, j);
+
+        for (var j = 31; j >= 0; j--, index++) bitArray[index] = IsBitSet(cl, j);
+
+        for (var j = 23; j >= 0; j--, index++) bitArray[index] = IsBitSet(n, j);
+
+        return bitArray;
+    }
+
+    // Get the header values from the compressed image
+    public static void GetHeader(this BitArray bitArray, ref int height, ref int width, ref int c0, ref int cl,
+        ref int n)
+    {
+        var index = 0;
+
+        for (var j = 11; j >= 0; j--, index++)
+            if (bitArray[index])
+                SetBit(ref height, j);
+
+        for (var j = 7; j >= 0; j--, index++)
+            if (bitArray[index])
+                SetBit(ref c0, j);
+
+        for (var j = 31; j >= 0; j--, index++)
+            if (bitArray[index])
+                SetBit(ref cl, j);
+
+        for (var j = 23; j >= 0; j--, index++)
+            if (bitArray[index])
+                SetBit(ref n, j);
+
+        width = n / height;
+    }
+
+    // Decompress the input file and save the decompressed image
+    public static Bitmap Decompress(this byte[] allBytes)
+    { 
+        var bitArray = new BitArray(allBytes);
+
+        bitArray.ReorderBytes();
+
+        int height = 0, width = 0, c0 = 0, cl = 0, n = 0;
+
+        bitArray.GetHeader(ref height, ref width, ref c0, ref cl, ref n);
+
+        var cList = new List<int>(new[] {c0});
+
+        for (var i = 1; i < n - 1; i++) cList.Add(0);
+
+        cList.Add(cl);
+
+        bitArray.Decode(ref cList, 0, n - 1);
+
+        var calculateN = cList.CalculateN(n);
+
+        var eList = calculateN.DecodeE();
+
+        var bitmap = ReversePredict(eList, height, width);
+
+        bitmap.Save($"DecompressedImage.bmp");
+
+        return bitmap;
+    }
+}
