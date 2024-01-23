5cbdf5b330f70e3c319eb32042033d8ddad71f79
bzorec <blaz.zorec@student.um.si>
2024-01-23 18:19:47 +0100

    added arff

--- Web/Direct4Me.UnitTests/ArffGenerator.cs
+++ Web/Direct4Me.UnitTests/ArffGenerator.cs
@@ -1,6 +1,98 @@
+using Xunit.Abstractions;
+
 namespace Direct4Me.UnitTests;
 
 public class ArffGenerator
 {
-    
+    private const int NumPackageBoxes = 50;
+    private const int NumInstances = 10_000;
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public ArffGenerator(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public void Generator()
+    {
+        var rand = new Random();
+
+        using var arffFile = new StreamWriter("delivery_data.arff");
+        arffFile.WriteLine("@relation delivery_data");
+        arffFile.WriteLine("@attribute boxId numeric"); // Attribute for box ID
+        arffFile.WriteLine("@attribute numPeopleHome {0,1,2,3,4,5}");
+        arffFile.WriteLine("@attribute paymentType {manual,prepaid}");
+        arffFile.WriteLine("@attribute needSignature {yes,no}");
+        arffFile.WriteLine("@attribute packageType {perishable,electronics,clothing,others}");
+        arffFile.WriteLine("@attribute deliveryUrgency {standard,expedited,immediate}");
+        arffFile.WriteLine("@attribute dayOfWeek {monday,tuesday,wednesday,thursday,friday,saturday,sunday}");
+        arffFile.WriteLine("@attribute repeatDelivery {yes,no}");
+        arffFile.WriteLine("@attribute postboxCapacity numeric");
+        arffFile.WriteLine("@attribute packagePickup {yes,no}");
+
+        arffFile.WriteLine("@data");
+
+        for (var instance = 0; instance < NumInstances; ++instance)
+        {
+            var boxId = rand.Next(1, NumPackageBoxes + 1); // Random box ID
+            var numPeopleHome = rand.Next(0, 6);
+            var paymentType = rand.NextDouble() > 0.5 ? "manual" : "prepaid";
+            var needSignature = rand.NextDouble() > 0.5 ? "yes" : "no";
+            var packageType = GetRandomPackageType(rand);
+            var deliveryUrgency = GetRandomDeliveryUrgency(rand);
+            var dayOfWeek = GetRandomDayOfWeek(rand);
+            var repeatDelivery = rand.NextDouble() > 0.5 ? "yes" : "no";
+            var capacityUsed = CalculateCapacityUsed(packageType);
+
+            arffFile.Write(
+                $"{boxId},{numPeopleHome},{paymentType},{needSignature},{packageType},{deliveryUrgency},{dayOfWeek},{repeatDelivery},{capacityUsed},");
+            arffFile.WriteLine(DiscretizePackagePickup(numPeopleHome, deliveryUrgency, repeatDelivery,
+                capacityUsed));
+
+            // Output statistics for each instance
+            _testOutputHelper.WriteLine("Instance " + (instance + 1) + " recorded");
+            _testOutputHelper.WriteLine("-------------------------------------------");
+        }
+    }
+
+    private static string DiscretizePackagePickup(int numPeopleHome, string deliveryUrgency,
+        string repeatDelivery, int totalCapacityUsed)
+    {
+        // Modify this logic as per your need for successful delivery
+        if (numPeopleHome > 0 && deliveryUrgency != "immediate" && repeatDelivery == "no" &&
+            totalCapacityUsed < 100) // Example capacity check
+            return "yes";
+        return "no";
+    }
+
+    private static string GetRandomPackageType(Random rand)
+    {
+        string[] types = { "perishable", "electronics", "clothing", "others" };
+        return types[rand.Next(types.Length)];
+    }
+
+    private static string GetRandomDeliveryUrgency(Random rand)
+    {
+        string[] urgencies = { "standard", "expedited", "immediate" };
+        return urgencies[rand.Next(urgencies.Length)];
+    }
+
+    private static string GetRandomDayOfWeek(Random rand)
+    {
+        string[] days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+        return days[rand.Next(days.Length)];
+    }
+
+    private static int CalculateCapacityUsed(string packageType)
+    {
+        // Implement logic to determine capacity used based on package type
+        return packageType switch
+        {
+            "perishable" => 5,
+            "electronics" => 10,
+            "clothing" => 3,
+            _ => 2
+        };
+    }
 }
